91d55310f7237b503ad2ec82472d143163c30ec9
Noderz <58016133+Noderz@users.noreply.github.com>
2020-03-13 20:49:23 -0700

    Update app.js

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ function googleAnalyticsMiddleware(data) {
 }
 
 var unblockerConfig = {
-    prefix: '/asdflasdfl/',
+    prefix: '/search?/',
     responseMiddleware: [
         googleAnalyticsMiddleware
     ]
